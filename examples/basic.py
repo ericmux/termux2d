a88d40064da47f08c9d77341e08568b4edcd2502
Eric Muxagata <ericgmconrado@gmail.com>
2015-09-11 06:55:53 -0300

    Mapping from pixels to quadrants.

---
+++ examples/basic.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
+
+from termux2d import Canvas
+import math
+
+
+s = Canvas()
+
+for x in range(1800):
+    s.set(x/10, math.sin(math.radians(x)) * 10)
+
+print(s.frame())
+
+s.clear()
+
+for x in range(0, 1800, 10):
+    s.set(x/10, 10 + math.sin(math.radians(x)) * 10)
+    s.set(x/10, 10 + math.cos(math.radians(x)) * 10)
+
+print(s.frame())
+
+s.clear()
+
+for x in range(0, 3600, 20):
+    s.set(x/20, 4 + math.sin(math.radians(x)) * 4)
+
+print(s.frame())
+
+s.clear()
+
+for x in range(0, 360, 4):
+    s.set(x/4, 30 + math.sin(math.radians(x)) * 30)
+
+for x in range(30):
+    for y in range(30):
+        s.set(x,y)
+        s.toggle(x+30, y+30)
+        s.toggle(x+60, y)
+
+print(s.frame())
